5504ec1b253f44fb6abfe559cff77a15929ecea5
Gretel Hilbing <ghilbing@gmail.com>
2021-09-13 11:11:39 -0700

    Callback for network response

--- app/src/main/java/com/hilbing/meals/ui/meals/MainActivity.kt
+++ app/src/main/java/com/hilbing/meals/ui/meals/MainActivity.kt
@@ -3,12 +3,18 @@ package com.hilbing.meals.ui.meals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.hilbing.meals.model.response.MealResponse
 import com.hilbing.meals.ui.theme.MealsTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +31,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MealsCategoriesScreen() {
     val viewModel: MealCategoriesViewModel = viewModel()
-    val meals = viewModel.getMeals()
-    Text(text = "Hello compose")
+    val rememberedMeals: MutableState<List<MealResponse>> = remember { mutableStateOf(emptyList<MealResponse>())}
+    viewModel.getMeals{
+        response ->
+        val mealsFromTheApi = response?.categories
+        rememberedMeals.value = mealsFromTheApi.orEmpty()
+
+    }
+    LazyColumn{
+        items(rememberedMeals.value){
+            meal ->
+            Text(text = meal.name)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/hilbing/meals/ui/meals/MealCategoriesViewModel.kt
+++ app/src/main/java/com/hilbing/meals/ui/meals/MealCategoriesViewModel.kt
@@ -3,10 +3,14 @@ package com.hilbing.meals.ui.meals
 import androidx.lifecycle.ViewModel
 import com.hilbing.meals.model.MealsRepository
 import com.hilbing.meals.model.response.MealResponse
+import com.hilbing.meals.model.response.MealsCategoriesResponse
 
 class MealCategoriesViewModel (private val repository: MealsRepository = MealsRepository()): ViewModel(){
-    fun getMeals(): List<MealResponse>{
-        return repository.getMeals().categories
+    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit){
+        repository.getMeals { response ->
+            successCallback(response)
+
+        }
     }
 
 }
